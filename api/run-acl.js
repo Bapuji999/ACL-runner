fe54c3b6639a2cc0eb9def0ced05bfc22dd71728
Bapuji Nayak <bapujinayak999@gmail.com>
2026-01-23 15:10:54 +0530

    Update run-acl.js

--- api/run-acl.js
+++ api/run-acl.js
@@ -19,10 +19,10 @@ export default async function handler(req, res) {
           grant_type: "authorization_code",
           client_id: "869dafaa-18b9-4020-a3da-6ceaffbcd6fe",
           scope: "https://m2itest.onmicrosoft.com/b2c-m2i-test/demo.read offline_access",
-          code: process.env.B2C_AUTH_CODE,
+          code: "eyJraWQiOiJjcGltY29yZV8wOTI1MjAxNSIsInZlciI6IjEuMCIsInppcCI6IkRlZmxhdGUiLCJzZXIiOiIxLjAifQ..PAZn2IVVUhNGzo2W.O3KT9iUCanqdZsc1HeTUwbxZ4K3ETGEpNbEwCDxqifKSKkW7QAG82jDwQC6Jq7BEwG_JJ3NiVwJA-nqF4Xqriy8WJg9dfF2ImrXa1b4jwyLu4eAH5kOzOJzC7cL9jGdjzZ4g6uAJlgEQtpjrZ30u7p5Qg0YJ-LekSdD86KgmgnfAqNnPDZVIGpqVVAT-BaPjS0jZC5AwaTLdg3aLKJsH86lBgB1ISUo3yAF4dkwjc8nnVL5JGU4Tx3BPDxENT92F-h4otSkt2gBFzdggYgeSybDYoDOuFNHPL2uWDrwVm5yi1s6QbpivfjQRCbPr39kzevRX_xfowf82ghI2b4DlAa7zwINd1ICV83Uziq9lyHwfblUy5sbKw9TtNjGO9qjvUUBYs0-sTI8QoG_NkrTmxKMhmrBLjTKBxbt4NJYi1mTXXfCu0TMNq6dnPd14shWGItTEeZxgXxCZ7srlTBMQ7oNGtCy2aO9wV8OdoQ1KzVXFMokcNEvtnABVP8GAMXNrds0xKXyZR1A8tqz35OJOGFQ4_vvIpN_0lIJ7bTlzXxhAIDWF9pkR8KkGETo8tPRB6N54MTfskMHGIpYah2kyNm06ImzhoLjprJBT6WezAYLunSxxBj2RNNqFlGYltYk_IHmal9Ji2lGInYhHip1qGcKDUb0t5TIgk5lLNntqvOKGnZ25N7tSY_ykNlY6IMxjfO_nHrB1Aadprt0fAPbMQF4QMFlPfDicdYp6QIidHBE43R313-NvERy043iJI7hEv8QyxnKPyA-m4w.tbmqu7odPtjwhLv_9a0Dkg",
           redirect_uri: "https://testservicing.azureedge.net/",
-          client_secret: process.env.B2C_CLIENT_SECRET,
-          code_verifier: process.env.B2C_CODE_VERIFIER
+          client_secret: "qu-8Q~GxfcGaae8-hpQiMTU18~eu3GJleFPh~aVQ",
+          code_verifier: "YTFjNjI1OWYzMzA3MTI4ZDY2Njg5M2RkNmVjNDE5YmEyZGRhOGYyM2IzNjdmZWFhMTQ1ODg3NDcxY2Nl"
         })
       }
     );
